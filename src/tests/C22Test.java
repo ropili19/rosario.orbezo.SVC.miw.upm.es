ee287d8e86e6a2864a6f01931a7a06f65188c7aa
ropili19 <rosariopili26@gmail.com>
2018-10-04 22:59:55 +0200

    C22Test

---
+++ src/tests/C22Test.java
@@ -0,0 +1,21 @@
+package src.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.C22;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class C22Test {
+    private C22 c22;
+
+    @BeforeEach
+    void before() {
+        c22 = new C22();
+    }
+
+    @Test
+    void testMA() {
+        assertEquals("mA", c22.mA());
+    }
+}
